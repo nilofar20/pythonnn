1880e660ced4efd2bbdd384d1edb61842b5994f2
nilofar20 <51949545+nilofar20@users.noreply.github.com>
2019-06-27 10:08:08 +0530

    Create vowell.py

---
+++ vowell.py
@@ -0,0 +1,7 @@
+l  = input()
+if l in ('a','e','i','o','u'):
+  print("Vowel")
+elif l in ('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
+  print("consonant")
+else:
+  print("invalid")
