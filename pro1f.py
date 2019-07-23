23d53d71e47acf7092147933da4ddc3a545f34c0
nilofar20 <51949545+nilofar20@users.noreply.github.com>
2019-07-23 15:08:46 +0530

    Create pro1f.py

---
+++ pro1f.py
@@ -0,0 +1,9 @@
+nil=int(input())
+array=list(map(int,input().split()))
+an=0
+for h in range(len(array)-2):
+    for z in range(h+1,len(array)-1):
+        for p in range(z+1,len(array)):
+            if array[h]<array[z]<array[p] and h<z<p:
+                an=an+p
+print(an)
