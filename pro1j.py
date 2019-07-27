71ecce2e0d712cce8813df4120c588a5bafff003
nilofar20 <51949545+nilofar20@users.noreply.github.com>
2019-07-27 13:34:13 +0530

    Create pro1j.py

---
+++ pro1j.py
@@ -0,0 +1,8 @@
+z1=int(input())
+chot=[int(o) for o in input().split(" ")]
+chet=0
+for p in range(z1):
+      for g in range(p):
+           if(chot[g]<chot[p]):
+                chet+=chot[g]
+print(chet)
